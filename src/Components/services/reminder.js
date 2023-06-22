f98e8e2fffb7aaccdbee02df163afe86ada89cc9
AdilLachin <adillachin929@gmail.com>
2023-06-22 10:32:29 +0600

    calander set time api issue fix

--- src/Components/services/reminder.js
+++ src/Components/services/reminder.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { follow_up_dev } from "./environment";
 
 export const handleAddFollowUp = async (followUpData) => {
   try {
     const result = await axios.post(
-      `${process.env?.REACT_APP_FOLLOWUP_URL}/api/follow-up`,
+      `${follow_up_dev}/api/follow-up`,
       followUpData
     );
     return result?.data;
@@ -15,7 +16,7 @@ export const handleAddFollowUp = async (followUpData) => {
 export const handleUpdateFollowUp = async (updatedFollowUpData, id) => {
   try {
     const result = await axios.put(
-      `${process.env?.REACT_APP_FOLLOWUP_URL}/api/follow-up-update/${id}`,
+      `${follow_up_dev}/api/follow-up-update/${id}`,
       updatedFollowUpData
     );
     return result?.data;
@@ -26,12 +27,9 @@ export const handleUpdateFollowUp = async (updatedFollowUpData, id) => {
 
 export const handleFetchFollowUp = async (userID) => {
   try {
-    const result = await axios.post(
-      `${process.env?.REACT_APP_FOLLOWUP_URL}/api/follow-up-by-user`,
-      {
-        user_id: userID,
-      }
-    );
+    const result = await axios.post(`${follow_up_dev}/api/follow-up-by-user`, {
+      user_id: userID,
+    });
     return result?.data;
   } catch (error) {
     return error.response;
@@ -41,7 +39,7 @@ export const handleFetchFollowUp = async (userID) => {
 // export const handleFetchReminder = async (userID) => {
 //   try {
 //     const result = await axios.get(
-//       `${process.env?.REACT_APP_FOLLOWUP_URL}/api/follow`,
+//       `${follow_up_dev}/api/follow`,
 //       {
 //         user_id: userID,
 //       }
@@ -55,7 +53,7 @@ export const handleFetchFollowUp = async (userID) => {
 export const handleDeleteFollowUp = async (followupID) => {
   try {
     const result = await axios.post(
-      `${process.env?.REACT_APP_FOLLOWUP_URL}/api/delete-notification`,
+      `${follow_up_dev}/api/delete-notification`,
       {
         id: followupID,
       }
