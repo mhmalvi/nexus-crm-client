97c7c607c79e204dbe63e27a628cf70b71257588
AdilLachin <adillachin929@gmail.com>
2023-07-12 17:08:06 +0600

    auth done

--- src/Components/services/notification.js
+++ src/Components/services/notification.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { NOTIFICATION_SERVER_DEV, crmNotification_dev } from "./environment";
-
+const authToken = JSON.parse(window.localStorage.getItem("auth_tok"));
 // export const handleFetchFollowUpNotification = async (userID) => {
 //   console.log("userID", userID);
 //   try {
@@ -33,12 +33,19 @@ import { NOTIFICATION_SERVER_DEV, crmNotification_dev } from "./environment";
 // };
 
 export const handleFetchNotificationList = async (uid) => {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + authToken,
+    },
+  };
   try {
     const result = await axios.post(
       `${crmNotification_dev}/api/${uid}/notifications-list`,
       {
-        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
-      }
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      },
+
+      config
     );
     return result?.data;
   } catch (error) {
@@ -47,10 +54,17 @@ export const handleFetchNotificationList = async (uid) => {
 };
 
 export const handleChangeNotificationStatus = async (notificationID) => {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + authToken,
+    },
+  };
   try {
     const result = await axios.post(
       // `${process.env.REACT_APP_NOTIFICATION_SERVER_URL}/api/change-status`,
-      `${crmNotification_dev}/api/change-status/${notificationID}`
+      `${crmNotification_dev}/api/change-status/${notificationID}`,
+      {},
+      config
     );
     return result?.data;
   } catch (error) {
