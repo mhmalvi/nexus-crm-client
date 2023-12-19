309a767e06fe60040560e5ad4633979800b90432
ZulkerQQ <zulker@quadque.tech>
2023-12-19 10:04:54 +0600

    !Update: Revamp landing Nav and header

--- src/Pages/LandingPage/Section1/SectionOne.jsx
+++ src/Pages/LandingPage/Section1/SectionOne.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./section1.css";
-import crm_show from "../../../assets/Gif/crm_show.gif";
-import backgroundphoto from "../../../assets/BackgroundPhotos/Background-01.png";
+// import crm_show from "../../../assets/Gif/crm_show.gif";
+// import backgroundphoto from "../../../assets/BackgroundPhotos/Background-01.png";
 import HeroSVG from "./HeroSVG";
+import Button from "../Components/Button";
 const SectionOne = () => {
   return (
     <div className="lg:min-h-[87vh] min-h-[95vh] w-full bg-[#180432] flex items-center justify-center">
@@ -26,162 +27,14 @@ const SectionOne = () => {
                 </label>
                 <input type="email" className="lg:w-1/2 w-[90vw] lg:rounded-r-[12px] rounded-[12px]" placeholder="Enter your email address"/>
               </div>
-              <form
-                className="hero-form"
-                style={{
-                  borderWidth: "0px",
-                  borderStyle: "solid",
-                  borderColor: "rgb(229, 231, 235)",
-                  boxSizing: "border-box",
-                  gap: "10px",
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: "space-between",
-                }}
-              >
-                <div
-                  style={{
-                    borderWidth: "0px",
-                    borderStyle: "solid",
-                    borderColor: "rgb(229, 231, 235)",
-                    boxSizing: "border-box",
-                    width: "100%",
-                    position: "relative",
-                    marginBottom: "24px",
-                  }}
-                >
-                  <input
-                    className="input"
-                    name="email"
-                    placeholder="Enter your email address"
-                    style={{
-                      borderWidth: "0px",
-                      borderStyle: "solid",
-                      borderColor: "rgb(229, 231, 235)",
-                      boxSizing: "border-box",
-                      margin: "0px",
-                      fontFamily: "inherit",
-                      fontSize: "100%",
-                      fontWeight: "inherit",
-                      lineHeight: "inherit",
-                      color: "inherit",
-                      border: "0px",
-                      borderRadius: "12px",
-                      padding: "20px",
-                      width: "65%",
-                      backgroundColor: "rgb(255, 255, 255)",
-                      boxShadow: "rgba(0, 0, 0, 0.15) 0px 0px 7px",
-                    }}
-                  />
-                </div>
-                <button
-                  className="button button-primary !bg-[#159afb]"
-                  type="submit"
-                  style={{
-                    borderWidth: "0px",
-                    borderStyle: "solid",
-                    borderColor: "rgb(229, 231, 235)",
-                    boxSizing: "border-box",
-                    margin: "0px",
-                    fontFamily: "inherit",
-                    lineHeight: "inherit",
-                    textTransform: "none",
-                    appearance: "button",
-                    backgroundImage: "none",
-                    border: "0px",
-                    fontSize: "18px",
-                    fontWeight: 600,
-                    cursor: "pointer",
-                    padding: "10px 20px",
-                    borderRadius: "12px",
-                    display: "flex",
-                    alignItems: "center",
-                    color: "rgb(255, 255, 255)",
-                    justifyContent: "center",
-                  }}
-                >
-                  Let's get started
-                  <span
-                    className="icon"
-                    style={{
-                      borderWidth: "0px",
-                      borderStyle: "solid",
-                      borderColor: "rgb(229, 231, 235)",
-                      boxSizing: "border-box",
-                      fontWeight: 500,
-                      marginTop: "3px",
-                      marginLeft: "5px",
-                      color: "rgb(255, 255, 255)",
-                      fontSize: "18px",
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                  >
-                    <svg
-                      height="1em"
-                      width="1em"
-                      fill="currentColor"
-                      stroke="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 16 16"
-                      xmlns="http://www.w3.org/2000/svg"
-                      style={{
-                        borderWidth: "0px",
-                        borderStyle: "solid",
-                        borderColor: "rgb(229, 231, 235)",
-                        boxSizing: "border-box",
-                        display: "block",
-                        verticalAlign: "middle",
-                      }}
-                    >
-                      <path
-                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                        fillRule="evenodd"
-                        style={{
-                          borderWidth: "0px",
-                          borderStyle: "solid",
-                          borderColor: "rgb(229, 231, 235)",
-                          boxSizing: "border-box",
-                        }}
-                      />
-                    </svg>
-                  </span>
-                </button>
-              </form>
-            </div>
-          </div>
-          <div
-            className="!col-span-12 lg:!col-span-7"
-            style={{
-              borderWidth: "0px",
-              borderStyle: "solid",
-              borderColor: "rgb(229, 231, 235)",
-              boxSizing: "border-box",
-              maxWidth: "100%",
-              paddingRight: "calc(1.5rem*.5)",
-              paddingLeft: "calc(1.5rem*.5)",
-              marginTop: "3rem",
-              flex: "0 0 auto",
-              flexShrink: 0,
-              width: "100%",
-            }}
-          >
-            <div
-              className="hero-banner w-full"
-              style={{
-                borderWidth: "0px",
-                borderStyle: "solid",
-                borderColor: "rgb(229, 231, 235)",
-                boxSizing: "border-box",
-              }}
-            >
-             
-             <HeroSVG/>
-            </div>
+              <Button title="Let's get started ->" variant={1}/>
+            </form>
           </div>
         </div>
+        <div className="lg:min-h-full flex lg:items-center items-start justify-center min-h-1/2 lg:mt-0 ">
+          <HeroSVG />
+        </div>
       </div>
-     
     </div>
   );
 };
