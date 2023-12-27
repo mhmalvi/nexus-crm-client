b3478e0e14d34c6b2f9c6d1e3454121f0f64189a
ZulkerQQ <zulker@quadque.tech>
2023-12-27 14:21:44 +0600

    !Update: Route to app.queleadscrm

--- src/Components/Shared/PrivateRoutes/ProtectedRoute.jsx
+++ src/Components/Shared/PrivateRoutes/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ const ProtectedRoute = () => {
   return Storage.getItem("auth_tok") ? (
     <Outlet />
   ) : (
-    <Navigate to={"/welcome"} />
+    window.location.assign("https://queleadscrm.com")
   );
 };
 
