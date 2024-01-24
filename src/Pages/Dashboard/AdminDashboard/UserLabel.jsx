e5741fea48587b6f56a2add5ab79da3aa1dba797
ZulkerQQ <zulker@quadque.tech>
2024-01-24 16:41:12 +0600

    !Fix: Update theme

--- src/Pages/Dashboard/AdminDashboard/UserLabel.jsx
+++ src/Pages/Dashboard/AdminDashboard/UserLabel.jsx
@@ -3,10 +3,13 @@ import { useSelector } from "react-redux";
 import Avatar from "react-avatar";
 const UserLabel = ({ setOpenProfile, openProfile }) => {
   const userDetails = useSelector((state) => state?.user);
+  const colorMode = useSelector((state) => state?.user)?.colorMode;
   return (
     <div
-      className={`ease-in duration-100 absolute group right-0 bg-${
-        openProfile ? "black" : "brand-color"
+      className={`ease-in duration-100 absolute group right-0 bg-gradient-to-b ${
+        colorMode
+          ? "from-[#100b1e] via-[#0b0815] to-[#000000] hover:from-[#7a51e3] hover:via-[#6e48cb] hover:to-[#55389f] "
+          : "from-[#7a51e3] hover:from-[#100b1e] via-[#6e48cb] hover:via-[#0b0815] to-[#55389f] hover:to-[#000000]"
       } m-1 p-1 rounded-lg shadow-md cursor-pointer hover:bg-black`}
       onClick={() => setOpenProfile(!openProfile)}
     >
