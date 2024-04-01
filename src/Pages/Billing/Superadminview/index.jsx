7157d005b7cfd7a8eed5e4b4ee48882abd89ac1b
Zulker Nien <zulkerb9b@gmail.com>
2024-04-01 10:23:12 +0600

    Check

--- src/Pages/Billing/Superadminview/index.jsx
+++ src/Pages/Billing/Superadminview/index.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { getAllSubscriptionList } from "../../../Components/services/billing";
+import { getAllProducts } from "../../../Components/services/billing";
+import SuperBillingForm from "./SuperBillingForm";
+
 const Superadminview = () => {
   const colorMode = useSelector((state) => state?.user)?.colorMode;
   const userDetails = useSelector((state) => state?.user);
   const [addMoreClicked, setAddMoreClicked] = useState(false);
+  const [productList, setProductList] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const response = await getAllSubscriptionList();
-      console.log(response);
+      const response = await getAllProducts();
+      setProductList(response);
     })();
-  });
+  }, []);
+
+  console.log(productList);
 
   return (
     <div className="flex flex flex-grow gap-4 w-full h-full mx-5 ">
       <div className="flex flex-col w-full h-full rounded-md shadow-md backdrop-blur-2xl bg-[#ffffff11] p-4">
         <div
-          className={`flex justify-between items-center bg-gray-800 w-full p-4 rounded-t-md`}
+          className={`flex justify-between items-center border ${
+            colorMode ? "border-brand-color" : "border-gray-800"
+          } bg-gray-800 w-full p-4 rounded-t-md`}
         >
           <h1 className={`text-slate-300 m-0 p-0`}>Packages</h1>
           <button
@@ -28,22 +35,28 @@ const Superadminview = () => {
           </button>
         </div>
         {addMoreClicked ? (
-          <div className="w-full h-full bg-brand-color rounded-b-md p-4">
-            FORM HERE
+          <div
+            className={`w-full h-full border ${
+              colorMode ? "border-brand-color" : "border-gray-800"
+            } rounded-b-md p-4`}
+          >
+            <SuperBillingForm />
           </div>
         ) : (
           <div className="w-full h-full bg-brand-color rounded-b-md p-4">
-            List here
+            {productList.data !== null ? (
+              productList?.data.map((item, index) => {
+                return <h1>{item.name}</h1>;
+              })
+            ) : (
+              <h1>No packages yet.</h1>
+            )}
           </div>
         )}
       </div>
+      {/* Price */}
       <div className="flex flex-col w-full h-full rounded-md shadow-md backdrop-blur-2xl bg-[#ffffff11] p-4">
         BILLING LIST
-        <hr />
-        (to be added in a later release)
-      </div>
-      <div className="flex flex-col w-full h-full rounded-md shadow-md backdrop-blur-2xl bg-[#ffffff11] p-4">
-        CUSTOMER LIST
         <hr />
         (to be added in a later release)
       </div>
