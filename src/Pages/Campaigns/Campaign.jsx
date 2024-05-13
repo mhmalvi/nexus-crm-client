e28ca7ffd8a5c6bc5cc552797ff27de8611b1051
ZulkerQQ <zulker@quadque.tech>
2024-05-13 15:24:18 +0600

    Schedule Mail test fix

--- src/Pages/Campaigns/Campaign.jsx
+++ src/Pages/Campaigns/Campaign.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import fbCampaignCover from "../../assets/Images/facebook-campaign.png";
+import fbCampaignCover from "../../assets/Images/facebook-campaign.svg";
 import { useSelector } from "react-redux";
 
 const Campaign = ({ campaign }) => {
@@ -10,53 +10,59 @@ const Campaign = ({ campaign }) => {
       to={`${campaign?.campaign_id}`}
       className="ease-in transition-all hover:scale-95 mx-auto w-64 h-92  flex flex-col items-center shadow cursor-pointer overflow-hidden rounded-md"
     >
-      <div className="relative">
-        <img src={fbCampaignCover} className="min-w-full" alt="Cover_Image" />
-        <div
-          className={`absolute -top-2.5 -left-11 w-29 flex justify-center items-center ${
-            campaign.campaign_status === "ACTIVE"
-              ? "bg-green-500"
-              : "bg-brand-color"
-          } bg-opacity-90 pb-1 pt-8 -rotate-45 shadow-md`}
-        >
-          <h6 className="text-slate-300 font-semibold font-poppins text-xs">
-            {campaign.campaign_status}
-          </h6>
-        </div>
-      </div>
-      <div
-        className={`flex h-full flex-col items-center rounded-b-md overflow-hidden justify-center font-poppins ${
-          colorMode ? "bg-slate-300" : "bg-gray-800"
-        }`}
-      >
-        <div className="flex flex-col justify-center items-center gap-4">
-          <h2
-            className={`m-0 px-4 py-2 font-semibold text-sm text-start border-b border-brand-color ${
-              colorMode ? "text-gray-800" : "text-slate-300"
+      {fbCampaignCover && (
+        <>
+          <div className="relative">
+            <img
+              src={fbCampaignCover}
+              className="min-w-full"
+              alt="Cover_Image"
+            />
+            <div
+              className={`absolute -top-2.5 -left-11 w-29 flex justify-center items-center ${
+                campaign.campaign_status === "ACTIVE"
+                  ? "bg-green-500"
+                  : "bg-brand-color"
+              } bg-opacity-90 pb-1 pt-8 -rotate-45 shadow-md`}
+            >
+              <h6 className="text-slate-300 font-semibold font-poppins text-xs">
+                {campaign.campaign_status}
+              </h6>
+            </div>
+          </div>
+          <div
+            className={`flex h-full w-full flex-col items-center justify-center rounded-b-md px-4 py-2 font-poppins ${
+              colorMode ? "bg-slate-300" : "bg-gray-800"
             }`}
           >
-            {campaign.campaign_name}
-          </h2>
-          <div className="w-full px-4 ">
-            <h2
-              className={`text-xs m-0 p-0 flex justify-between ${
-                colorMode ? "text-gray-800" : "text-slate-300"
-              }`}
-            >
-              <span>
-                {new Date(campaign.start_time).toISOString().split("T")[0]}
-              </span>
-              <span>-</span>
-              <span>
-                {new Date(campaign.stop_time).toISOString().split("T")[0]}
-              </span>
-            </h2>
-          </div>
-        </div>
-        {/* <h2 className="text-justify text-xs overflow-hidden h-19 px-2">
+            <div className="flex flex-col justify-center items-center gap-4">
+              <h2
+                className={`max-w-[220px] overflow-hidden m-0 font-semibold text-sm text-start border-b border-brand-color ${
+                  colorMode ? "text-gray-800" : "text-slate-300"
+                }`}
+              >
+                {campaign.campaign_name}
+              </h2>
+              <h2
+                className={`w-full text-xs m-0 p-0 flex justify-between ${
+                  colorMode ? "text-gray-800" : "text-slate-300"
+                }`}
+              >
+                <span>
+                  {new Date(campaign.start_time).toISOString().split("T")[0]}
+                </span>
+                <span>-</span>
+                <span>
+                  {new Date(campaign.stop_time).toISOString().split("T")[0]}
+                </span>
+              </h2>
+            </div>
+            {/* <h2 className="text-justify text-xs overflow-hidden h-19 px-2">
           {campaign.details}
         </h2> */}
-      </div>
+          </div>
+        </>
+      )}
     </Link>
   );
 };
